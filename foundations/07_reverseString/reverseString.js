8d006ceb5b9828a615b1eb514777249913361303
Cadenzathena <justinaddeh@gmail.com>
2026-04-26 20:18:24 +0100

    Update reverseString to pass spec test

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arr = string.split('');
+    let modifiedArr = arr.reverse();
+    return modifiedArr.join('');
 };
+
+reverseString('hello there');
 
 // Do not edit below this line
 module.exports = reverseString;
