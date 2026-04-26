69f2195502da888bdd7ca4ac07e0ed3f7ae3109a
Cadenzathena <justinaddeh@gmail.com>
2026-04-26 19:52:37 +0100

    Update repeatStrings to pass spec test

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(stringArg, numArg) {
+    if (numArg < 0) {
+        return "ERROR";
+    }
+    else if (numArg >= 0) {
+        let arr = [];
 
+        for (let i = 1; i <= numArg; i++) {
+            arr.push(stringArg);
+        }
+
+        return arr.join("");
+    } 
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
