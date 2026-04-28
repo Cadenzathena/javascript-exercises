8b6d4dcf8ce85176a819af6e46a68bfe5a43a708
Cadenzathena <justinaddeh@gmail.com>
2026-04-28 10:20:21 +0100

    Update removeFromArray to pass spec test

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr,...itemsToRemove) {
+    return arr.filter((item) => !itemsToRemove.includes(item));
 };
+
+console.log(removeFromArray([1, 2, 3, 4, "catch", "frog"], 2, 2, 2, 3, 8, 9, "frog"));
 
 // Do not edit below this line
 module.exports = removeFromArray;
