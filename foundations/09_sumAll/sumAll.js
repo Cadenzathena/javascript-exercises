43550d6034e60f674d55074f8eb212854644a153
Cadenzathena <justinaddeh@gmail.com>
2026-04-28 12:10:32 +0100

    Update sumAll to pass spec test

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,23 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let rangedArray = [];
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR";
+    }
+    else if (num1 < 0 || num2 < 0) {
+        return "ERROR";
+    }
+    else {
+        for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+        rangedArray.push(i);
+    }
+
+    console.log(rangedArray);
+    return rangedArray.reduce((accumulator, current) => accumulator + current);
+    }
 };
+
+console.log(sumAll(17.5, -3));
 
 // Do not edit below this line
 module.exports = sumAll;
