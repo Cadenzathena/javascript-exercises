c9b455b3e6029ecab8f2f3b1a494f848af113ceb
Cadenzathena <justinaddeh@gmail.com>
2026-04-30 16:20:23 +0100

    Update tempConversion to pass spec test

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahNum) {
+  let celsiusValue = (fahNum - 32) * (5 / 9);
+  return (Math.round(celsiusValue * 10) / 10);
 };
 
-const convertToFahrenheit = function() {
+convertToCelsius(32);
+
+const convertToFahrenheit = function(celNum) {
+  let fahrenheitValue = (celNum * (9 / 5)) + 32;
+  return (Math.round(fahrenheitValue * 10) / 10);
 };
+
+convertToFahrenheit(0);
 
 // Do not edit below this line
 module.exports = {
