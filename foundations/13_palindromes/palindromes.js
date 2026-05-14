c37be43cfe16f2a66d33c83e2aa63f72cb067ad3
Cadenzathena <justinaddeh@gmail.com>
2026-05-14 18:16:45 +0100

    Update palindromes.js to pass the spec test

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let cleansedString = string.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()?"'’]/g, "");
+    cleansedString = cleansedString.replace(/ /g, "");
+    
+    const stringPartOne = cleansedString.slice(0, Math.floor(cleansedString.length / 2))
+    const stringPartTwo = cleansedString.slice(Math.ceil(cleansedString.length / 2), string.length)
+        .split('')
+        .reverse()
+        .join('')
 
+    return stringPartOne === stringPartTwo ? true : false;
 };
 
 // Do not edit below this line
