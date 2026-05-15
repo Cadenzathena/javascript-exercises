046ea3026c4eb8e26d4c85901215e66e7091de93
Cadenzathena <justinaddeh@gmail.com>
2026-05-15 11:41:28 +0100

    Update fibonacci to pass spec test

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,29 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const fibonacciArr = [1, 1];
+    let fibonacciIndexer = 0;
+    num = +num;
 
+    switch (num) {
+        case 0:
+            return 0;
+
+        case 1 || 2:
+            return 1;
+    }
+
+    switch (Math.sign(num)) {
+        case -1:
+            return "OOPS"
+    }
+
+    for (let i = 2 ; i <= num ; i++) {
+        fibonacciArr.push(fibonacciArr[0 + fibonacciIndexer]
+                        + fibonacciArr[1 + fibonacciIndexer]);
+
+        fibonacciIndexer++;
+    }
+
+    return fibonacciArr[num - 1];
 };
 
 // Do not edit below this line
