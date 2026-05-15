fc66ac248bba4c203c17c9e15b928692c7cea07f
Cadenzathena <justinaddeh@gmail.com>
2026-05-15 13:02:14 +0100

    Update findTheOldest to pass the spec test

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    arr.map((item) => {
+        if (!item.yearOfDeath) {
+            item.yearOfDeath = new Date().getFullYear();
+        }
+    })
 
+    let ageArr = arr.map((item) => item.yearOfDeath - item.yearOfBirth);
+
+    return arr[ageArr.indexOf(Math.max(...ageArr))];
 };
 
 // Do not edit below this line
